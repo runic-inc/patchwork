ec39301d23f4ca799b02b62e6dde3f359540e0d7
Rob Green <rbgrn@rbgrn.net>
2023-09-25 11:31:43 -0700

    and ERC165

--- test/PatchworkNFTInterface.t.sol
+++ test/PatchworkNFTInterface.t.sol
@@ -11,21 +11,19 @@ import "../src/sampleNFTs/TestFragmentLiteRefNFT.sol";
 contract PatchworkNFTInterfaceTest is Test {
     TestPatchLiteRefNFT testPatchLiteRefNFT;
     TestFragmentLiteRefNFT testFragmentLiteRefNFT;
-    bytes4 ERC165ID;
 
     function setUp() public {
         testPatchLiteRefNFT = new TestPatchLiteRefNFT(address(1));
         testFragmentLiteRefNFT = new TestFragmentLiteRefNFT(0x0000000000000000000000000000000000000000);
-        ERC165ID = bytes4(0x01ffc9a7);
     }
 
     function testSupportsInterface() public {
-        assertTrue(testPatchLiteRefNFT.supportsInterface(ERC165ID));
+        assertTrue(testPatchLiteRefNFT.supportsInterface(type(IERC165).interfaceId));
         assertTrue(testPatchLiteRefNFT.supportsInterface(type(IPatchworkNFT).interfaceId));
         assertTrue(testPatchLiteRefNFT.supportsInterface(type(IPatchworkLiteRef).interfaceId));
         assertTrue(testPatchLiteRefNFT.supportsInterface(type(IPatchworkPatch).interfaceId));
 
-        assertTrue(testFragmentLiteRefNFT.supportsInterface(ERC165ID));
+        assertTrue(testFragmentLiteRefNFT.supportsInterface(type(IERC165).interfaceId));
         assertTrue(testFragmentLiteRefNFT.supportsInterface(type(IPatchworkNFT).interfaceId));
         assertTrue(testFragmentLiteRefNFT.supportsInterface(type(IPatchworkAssignableNFT).interfaceId));
     }
