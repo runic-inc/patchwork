05db314661962ea8e0c96b85d0f6a9412b038327
Rob Green <rbgrn@rbgrn.net>
2023-10-06 14:49:36 -0700

    wip

--- src/sampleNFTs/TestAccountPatchNFT.sol
+++ src/sampleNFTs/TestAccountPatchNFT.sol
@@ -6,12 +6,16 @@ import "../PatchworkAccountPatch.sol";
 contract TestAccountPatchNFT is PatchworkAccountPatch {
 
     uint256 _nextTokenId = 0;
+    bool _sameOwnerModel;
+    bool _transferrable;
 
     struct TestPatchworkNFTMetadata {
         uint256 thing;
     }
 
-    constructor(address manager_) PatchworkNFT("testscope", "TestAccountPatchNFT", "TPLR", msg.sender, manager_) {
+    constructor(address manager_, bool sameOwnerModel_, bool transferrable_) PatchworkNFT("testscope", "TestAccountPatchNFT", "TPLR", msg.sender, manager_) {
+        _sameOwnerModel = sameOwnerModel_;
+        _transferrable = transferrable_;
     }
 
     function schemaURI() pure external returns (string memory) {
@@ -29,11 +33,30 @@ contract TestAccountPatchNFT is PatchworkAccountPatch {
     }
 
     function mintPatch(address to, address original) public returns (uint256) {
+        if (_sameOwnerModel) {
+            if (to != original) {
+                revert();
+            }
+        }
         uint256 tokenId = _nextTokenId;
         _nextTokenId++;
         _storePatch(tokenId, original);
         _mint(to, tokenId);
         _metadataStorage[tokenId] = new uint256[](1);
         return tokenId;
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 firstTokenId,
+        uint256 batchSize
+    ) internal override {
+        if (!_transferrable) {
+            // allow burn only
+            if (to != address(0)) {
+                revert();
+            }
+       }
     }
 }
