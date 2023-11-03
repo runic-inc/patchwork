2d58011c898c53fadbddbb6800da3ea27a873d36
Rob Green <rbgrn@rbgrn.net>
2023-11-03 11:18:29 -0700

    Moved test NFTs and fixed linter with proto test

--- test/nfts/TestFragmentLiteRefNFT.sol
+++ test/nfts/TestFragmentLiteRefNFT.sol
@@ -9,88 +9,92 @@ pragma solidity ^0.8.13;
   Has metadata as defined in totem-metadata.json
 */
 
-import "../PatchworkPatch.sol";
-import "../PatchworkLiteRef.sol";
+import "../../src/PatchworkFragmentSingle.sol";
+import "../../src/PatchworkLiteRef.sol";
 
-struct TestPatchLiteRefNFTMetadata {
-    uint64[8] artifactIDs;
-    uint16 xp;
-    uint8 level;
-    uint16 xpLost;
-    uint16 stakedMade;
-    uint16 stakedCorrect;
-    uint8 evolution;
-    string nickname;
+enum FragmentType {
+    BASE,
+    MOUTH,
+    EYES,
+    HAT
 }
 
-contract TestPatchLiteRefNFT is PatchworkPatch, PatchworkLiteRef {
+struct TestFragmentLiteRefNFTMetadata {
+    uint64[8] artifactIDs;
+    FragmentType fragmentType;
+    uint8 rarity;
+    string name;
+}
+
+contract TestFragmentLiteRefNFT is PatchworkFragmentSingle, PatchworkLiteRef {
 
     uint256 _nextTokenId;
+    bool _testLockOverride;
+    bool _getLiteRefOverrideSet;
+    uint64 _getLiteRefOverride;
+    bool _getAssignedToOverrideSet;
+    address _getAssignedToOverride;
 
-    constructor(address manager_) PatchworkNFT("testscope", "TestPatchLiteRef", "TPLR", msg.sender, manager_) PatchworkLiteRef() {
+    constructor (address _manager) PatchworkNFT("testscope", "TestFragmentLiteRef", "TFLR", msg.sender, _manager) {
     }
 
     // ERC-165
-    function supportsInterface(bytes4 interfaceID) public view virtual override(PatchworkPatch, PatchworkLiteRef) returns (bool) {
-        return PatchworkLiteRef.supportsInterface(interfaceID) ||
-            PatchworkPatch.supportsInterface(interfaceID);        
+    function supportsInterface(bytes4 interfaceID) public view virtual override(PatchworkFragmentSingle, PatchworkLiteRef) returns (bool) {
+        return PatchworkLiteRef.supportsInterface(interfaceID) || 
+            PatchworkFragmentSingle.supportsInterface(interfaceID);             
     }
 
-    function schemaURI() pure external override returns (string memory) {
-        return "https://mything/my-metadata.json";
+    function mint(address to) external returns (uint256 tokenId) {
+        tokenId = _nextTokenId;
+        _nextTokenId++;
+        _safeMint(to, tokenId);
+        _metadataStorage[tokenId] = new uint256[](3);
     }
 
-    function imageURI(uint256 _tokenId) pure external override returns (string memory) {}
-
-    function setManager(address manager_) external {
-        require(_checkWriteAuth());
-        _manager = manager_;
+    function schemaURI() pure external returns (string memory) {
+        return "https://mything/my-fragment-metadata.json";
     }
 
+    function imageURI(uint256 _tokenId) pure external returns (string memory) {
+        return string(abi.encodePacked("https://mything/fragment-", _tokenId));
+    }
+    
     /*
     Hard coded prototype schema is:
-    slot 0 offset 0 = artifactIDs (spans 2) - also we need special built-in handling for < 256 bit IDs
-    slot 2 offset 0 = xp
-    slot 2 offset 16 = level
-    slot 2 offset 24 = xpLost
-    slot 2 offset 40 = stakedMade
-    slot 2 offset 56 = stakedCorrect
-    slot 2 offset 72 = evolution
-    slot 2 offset 80 = nickname
+    slot 0 offset 0 = fragmentType
+    slot 0 offset 8 = rarity
+    slot 0 offset 16 = name
+    slot 0 offset 144 = <next open>
     */
-    function schema() pure external override returns (MetadataSchema memory) {
+    function schema() pure external returns (MetadataSchema memory) {
         MetadataSchemaEntry[] memory entries = new MetadataSchemaEntry[](8);
         entries[0] = MetadataSchemaEntry(0, 0, FieldType.UINT64, 8, FieldVisibility.PUBLIC, 0, 0, "artifactIDs");
-        entries[1] = MetadataSchemaEntry(1, 1, FieldType.UINT16, 0, FieldVisibility.PUBLIC, 2, 0, "xp");
-        entries[2] = MetadataSchemaEntry(2, 2, FieldType.UINT8, 0, FieldVisibility.PUBLIC, 2, 16, "level");
-        entries[3] = MetadataSchemaEntry(3, 0, FieldType.UINT16, 0, FieldVisibility.PUBLIC, 2, 24, "xpLost");
-        entries[4] = MetadataSchemaEntry(4, 0, FieldType.UINT16, 0, FieldVisibility.PUBLIC, 2, 40, "stakedMade");
-        entries[5] = MetadataSchemaEntry(5, 0, FieldType.UINT16, 0, FieldVisibility.PUBLIC, 2, 56, "stakedCorrect");
-        entries[6] = MetadataSchemaEntry(6, 0, FieldType.UINT8, 0, FieldVisibility.PUBLIC, 2, 72, "evolution");
-        entries[7] = MetadataSchemaEntry(7, 0, FieldType.CHAR16, 0, FieldVisibility.PUBLIC, 2, 80, "nickname");
+        entries[1] = MetadataSchemaEntry(1, 0, FieldType.UINT8, 0, FieldVisibility.PUBLIC, 2, 0, "fragmentType");
+        entries[2] = MetadataSchemaEntry(2, 0, FieldType.UINT16, 0, FieldVisibility.PUBLIC, 2, 8, "rarity");
+        entries[3] = MetadataSchemaEntry(3, 0, FieldType.CHAR16, 0, FieldVisibility.PUBLIC, 2, 16, "name");
         return MetadataSchema(1, entries);
     }
 
-    function packMetadata(TestPatchLiteRefNFTMetadata memory data) public pure returns (uint256[] memory slots) {
-        bytes32 nickname;
-        bytes memory ns = bytes(data.nickname);
+    function packMetadata(TestFragmentLiteRefNFTMetadata memory data) public pure returns (uint256[] memory slots) {
+        bytes32 _name;
+        bytes memory ns = bytes(data.name);
 
         assembly {
-            nickname := mload(add(ns, 32))
+            _name := mload(add(ns, 32))
         }
         slots = new uint256[](3);
         slots[0] = uint256(data.artifactIDs[0]) | uint256(data.artifactIDs[1]) << 64 | uint256(data.artifactIDs[2]) << 128 | uint256(data.artifactIDs[3]) << 192;
         slots[1] = uint256(data.artifactIDs[4]) | uint256(data.artifactIDs[5]) << 64 | uint256(data.artifactIDs[6]) << 128 | uint256(data.artifactIDs[7]) << 192;
-        slots[2] = uint256(data.xp) | uint256(data.level) << 16 | uint256(data.xpLost) << 24 | uint256(data.stakedMade) << 40 | uint256(data.stakedCorrect) << 56 | uint256(data.evolution) << 72 | uint256(nickname) >> 128 << 80;
+        slots[2] = uint256(data.fragmentType) | uint256(data.rarity) << 8 | uint256(_name) >> 128 << 16;
         return slots;
     }
 
-    function storeMetadata(uint256 _tokenId, TestPatchLiteRefNFTMetadata memory data) public {
+    function storeMetadata(uint256 _tokenId, TestFragmentLiteRefNFTMetadata memory data) public {
         require(_checkTokenWriteAuth(_tokenId), "not authorized");
         _metadataStorage[_tokenId] = packMetadata(data);
     }
 
-    function unpackMetadata(uint256[] memory slots) public pure returns (TestPatchLiteRefNFTMetadata memory data) {
+    function unpackMetadata(uint256[] memory slots) public pure returns (TestFragmentLiteRefNFTMetadata memory data) {
         data.artifactIDs[0] = uint64(slots[0]);
         data.artifactIDs[1] = uint64(slots[0] >> 64);
         data.artifactIDs[2] = uint64(slots[0] >> 128);
@@ -99,47 +103,17 @@ contract TestPatchLiteRefNFT is PatchworkPatch, PatchworkLiteRef {
         data.artifactIDs[5] = uint64(slots[1] >> 64);
         data.artifactIDs[6] = uint64(slots[1] >> 128);
         data.artifactIDs[7] = uint64(slots[1] >> 192);
-        data.xp = uint16(slots[2]);
-        data.level = uint8(slots[2] >> 16);
-        data.xpLost = uint16(slots[2] >> 24);
-        data.stakedMade = uint16(slots[2] >> 40);
-        data.stakedCorrect = uint16(slots[2] >> 56);
-        data.evolution = uint8(slots[2] >> 72);
-        data.nickname = string(abi.encodePacked(bytes16(uint128(slots[2] >> 80))));
+        data.fragmentType = FragmentType(slots[2]);
+        data.rarity = uint8(slots[2] >> 8);
+        data.name = string(abi.encodePacked(bytes16(uint128(slots[2] >> 16))));
         return data;
     }
 
-    function loadMetadata(uint256 _tokenId) public view returns (TestPatchLiteRefNFTMetadata memory data) {
+    function loadMetadata(uint256 _tokenId) public view returns (TestFragmentLiteRefNFTMetadata memory data) {
         return unpackMetadata(_metadataStorage[_tokenId]);
     }
 
-    // Store Only XP
-    function storeXP(uint256 _tokenId, uint16 xp) public {
-        require(_checkTokenWriteAuth(_tokenId) || _permissionsAllow[msg.sender] & 0x1 == 1, "not authorized");
-        // Slot 2 offset 0: 16 bit value
-        uint256 cleared = uint256(_metadataStorage[_tokenId][2]) & 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF0000;
-        _metadataStorage[_tokenId][2] = cleared | uint256(xp);
-    }
-
-    // Load Only XP
-    function loadXP(uint256 _tokenId) public view returns (uint16) {
-        return uint16(uint256(_metadataStorage[_tokenId][2]));
-    }
-
-    // Store Only level
-    function storeLevel(uint256 _tokenId, uint8 level) public {
-        require(_checkTokenWriteAuth(_tokenId) || _permissionsAllow[msg.sender] & 0x2 == 2, "not authorized");
-        // Slot 2 offset 16: 16 bit value
-        uint256 cleared = uint256(_metadataStorage[_tokenId][2]) & 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF00FFFF;
-        _metadataStorage[_tokenId][2] = cleared | (uint256(level) << 16);
-    }
-
-    // Load Only level
-    function loadLevel(uint256 _tokenId) public view returns (uint16) {
-        return uint16(uint256(_metadataStorage[_tokenId][2]) >> 16);
-    }
-
-    function addReference(uint256 ourTokenId, uint64 referenceAddress) public override {
+   function addReference(uint256 ourTokenId, uint64 referenceAddress) public override {
         require(_checkTokenWriteAuth(ourTokenId), "not authorized");
         uint256[] storage mdStorage = _metadataStorage[ourTokenId];
         uint256 slot = mdStorage[0];
@@ -168,19 +142,6 @@ contract TestPatchLiteRefNFT is PatchworkPatch, PatchworkLiteRef {
     function batchAddReferences(uint256 ourTokenId, uint64[] calldata /*_referenceAddresses*/) public view override {
         require(_checkTokenWriteAuth(ourTokenId), "not authorized");
         // TODO bulk insert for fewer stores
-    }
-
-    function mintPatch(address originalNFTOwner, address originalNFTAddress, uint originalNFTTokenId) external returns (uint256 tokenId){
-        if (msg.sender != _manager) {
-            revert();
-        }
-        // Just for testing
-        tokenId = _nextTokenId;
-        _nextTokenId++;
-        _storePatch(tokenId, originalNFTAddress, originalNFTTokenId);
-        _safeMint(originalNFTOwner, tokenId);
-        _metadataStorage[tokenId] = new uint256[](3);
-        return tokenId;
     }
 
     function removeReference(uint256 ourTokenId, uint64 referenceAddress) public override {
@@ -231,13 +192,53 @@ contract TestPatchLiteRefNFT is PatchworkPatch, PatchworkLiteRef {
         }
         return (addresses, tokenIds);
     }
-    
+
     function _checkWriteAuth() internal override(PatchworkNFT, PatchworkLiteRef) view returns (bool allow) {
         return PatchworkNFT._checkWriteAuth();
     }
 
-    function burn(uint256 tokenId) public {
-        // test only
-        _burn(tokenId);
+    // Function for mocking test behaviors - set to true for it to return unlocked always
+    function setTestLockOverride(bool override_) public {
+        _testLockOverride = override_;
+    }
+
+    // Override to check bad behavior cases
+    function locked(uint256 tokenId) public view virtual override returns (bool) {
+        if (_testLockOverride) {
+            return false;
+        }
+        return super.locked(tokenId);
+    }
+
+    // Testing overrides
+    function setGetLiteRefOverride(bool set_, uint64 value_) public {
+        _getLiteRefOverrideSet = set_;
+        _getLiteRefOverride = value_;
+    }
+
+    // Testing overrides
+    function getLiteReference(address addr, uint256 tokenId) public virtual override view returns (uint64 referenceAddress, bool redacted) {
+        if (_getLiteRefOverrideSet) {
+            return (_getLiteRefOverride, false);
+        }
+        return super.getLiteReference(addr, tokenId);
+    }
+
+    // Testing overrides
+    function setGetAssignedToOverride(bool set_, address value_) public {
+        _getAssignedToOverrideSet = set_;
+        _getAssignedToOverride = value_;
+    }
+
+    function getAssignedTo(uint256 ourTokenId) public virtual override view returns (address, uint256) {
+        if (_getAssignedToOverrideSet) {
+            return (_getAssignedToOverride, 1);
+        }
+        return super.getAssignedTo(ourTokenId);
+    }
+
+    function setScopeName(string memory scopeName) public {
+        // For testing only
+        _scopeName = scopeName;
     }
 }
