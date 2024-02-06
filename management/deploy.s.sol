cf5da93ce8ef064f5e7372708df78849e2626dae
kevinday <kevin.r.day@gmail.com>
2024-02-06 12:17:48 -0500

    default CREATE2 deploy (#93)
    Co-authored-by: Kevin Day <kevin.r.day@gmail.comm>

--- management/deploy.s.sol
+++ management/deploy.s.sol
@@ -8,25 +8,15 @@ import { PatchworkProtocolAssigner } from "../src/PatchworkProtocolAssigner.sol"
 
 contract DeterministicPatchworkDeploy is Script {
 
-    address internal constant _DETERMINISTIC_CREATE2_FACTORY = 0x7A0D94F55792C434d74a40883C6ed8545E406D12;
-
     function run() public {
         address patchworkOwner = vm.envAddress("PATCHWORK_OWNER");
         vm.startBroadcast();
 
-        bytes memory creationCode = type(PatchworkProtocolAssigner).creationCode;
-        bytes memory creationBytecode = abi.encodePacked(creationCode, abi.encode(patchworkOwner));
-        (bool success, bytes memory returnData) = _DETERMINISTIC_CREATE2_FACTORY.call(creationBytecode);
-        require(success, "Failed to deploy Assigner Module");
-        address assignerAddress = address(uint160(bytes20(returnData)));
-        console.log("Deployed Assigner module contract at: ", assignerAddress);
-
-        creationCode = type(PatchworkProtocol).creationCode;
-        creationBytecode = abi.encodePacked(creationCode, abi.encode(patchworkOwner), abi.encode(assignerAddress));
-        (success, returnData) = _DETERMINISTIC_CREATE2_FACTORY.call(creationBytecode);
-        require(success, "Failed to deploy Patchwork");
-        address patchworkAddress = address(uint160(bytes20(returnData)));
-        console.log("Deployed Patchwork contract at: ", patchworkAddress);
+        bytes32 salt = "1";
+        PatchworkProtocolAssigner ppAssigner = new PatchworkProtocolAssigner{salt:salt}(patchworkOwner);
+        console.log(address(ppAssigner));
+        PatchworkProtocol pp = new PatchworkProtocol{salt:salt}(patchworkOwner, address(ppAssigner));
+        console.log(address(pp));
 
         vm.stopBroadcast();
     }
