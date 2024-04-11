0a1191d9055294bcfddabc8e751a9057b39f7b3b
Kevin Day <kevin.r.day@gmail.comm>
2024-04-11 11:58:25 -0400

    base-sepolia config

--- management/deploy.sh
+++ management/deploy.sh
@@ -5,7 +5,7 @@
 # Usage instructions
 if [ $# -lt 1 ]; then
   echo "Usage: $0 <network> [--broadcast]"
-  echo "Supported networks: base, sepolia"
+  echo "Supported networks: base, sepolia, base-sepolia"
   exit 1
 fi
 
@@ -20,6 +20,8 @@ fi
 NETWORK=$1
 PATCHWORK_OWNER=""
 RPC_URL=""
+ASSSIGNER_SALT=0x3feab664aac5550765cdda720dd10a2874a6360108b55e5621d332ed92bacd8a
+PATCHWORK_SALT=0x3feab664aac5550765cdda720dd10a2874a63601e0d0bf13a0f99b10db97638b
 
 case $NETWORK in
   "base")
@@ -29,6 +31,10 @@ case $NETWORK in
   "sepolia")
     PATCHWORK_OWNER=$SEPOLIA_PATCHWORK_OWNER
     RPC_URL=$SEPOLIA_RPC_URL
+    ;;
+  "base-sepolia")
+    PATCHWORK_OWNER=$BASE_SEPOLIA_PATCHWORK_OWNER
+    RPC_URL=$BASE_SEPOLIA_RPC_URL
     ;;
   *)
     echo "Network not supported"
